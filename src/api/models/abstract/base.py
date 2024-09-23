b68aa106089c5bf48acfee44930964d444b7a271
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-09-23 16:04:31 +0500

    SoftDelete applied to base model so all new models objects will be softly deleted

--- src/api/models/abstract/base.py
+++ src/api/models/abstract/base.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 from api.models.mixins.uid import UIDMixin
+from django_softdelete.models import SoftDeleteModel
 
 
-class BaseModel(models.Model, UIDMixin):
+class BaseModel(SoftDeleteModel, UIDMixin):
     id = models.CharField(max_length=15, primary_key=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
+        permissions = [("can_undelete", "Can undelete this object")]
 
     def save(self, *args, **kwargs):
         self.set_uid()
