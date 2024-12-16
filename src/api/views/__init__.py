1752c4780c63152016294b211c27f369c963dab2
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-12-16 13:25:06 +0500

    view for OTP added

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -4,10 +4,12 @@ from api.views.outlets.outlet import (
 )
 from api.views.outlets.merchant_member import MerchantMemberListCreateView
 from api.views.permissions import PermissionsListAPIView
+from api.views.otp import OTPView
 
 __all__ = [
     "OutletListCreateView",
     "OutletRetrieveUpdateDestroyView",
     "MerchantMemberListCreateView",
     "PermissionsListAPIView",
+    "OTPView",
 ]
