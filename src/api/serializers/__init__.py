cabfac62bee6cf807fe6464e6c9d192c44bbbaa1
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-12-16 13:23:41 +0500

    serializer for OTP added

--- src/api/serializers/__init__.py
+++ src/api/serializers/__init__.py
@@ -1,9 +1,10 @@
+from api.serializers.otp import OTPSerializer
 from api.serializers.user import UserSerializer
 from api.serializers.outlet import OutletSerializer
-from api.serializers.lookup import LookupSerializer, LookupOperationSerializer
 from api.serializers.merchant import MerchantSerializer
-from api.serializers.merchant_member import MerchantMemberSerializer
 from api.serializers.permissions import PermissionSerializer
+from api.serializers.merchant_member import MerchantMemberSerializer
+from api.serializers.lookup import LookupSerializer, LookupOperationSerializer
 
 __all__ = [
     "UserSerializer",
@@ -13,4 +14,5 @@ __all__ = [
     "MerchantMemberSerializer",
     "LookupOperationSerializer",
     "PermissionSerializer",
+    "OTPSerializer",
 ]
