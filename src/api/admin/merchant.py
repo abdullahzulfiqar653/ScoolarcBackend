ab618a0355e952b733211130ebdab3c422514649
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-12-16 13:19:24 +0500

    admin updatd for different modela  d according to structure

---
+++ src/api/admin/merchant.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib import admin
+from api.models.lookup import Lookup
+from api.models.merchant import Merchant
+from api.models.outlet import Outlet
+from api.models.merchant_member import MerchantMember
+
+admin.site.register(Outlet)
+
+
+class MerchantMemberForm(forms.ModelForm):
+    class Meta:
+        model = MerchantMember
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Filter the Lookup choices for the role field (filtering by "Role" type)
+        role_type = Lookup.objects.get(
+            name="Role"
+        )  # Assuming "Role" is the parent type
+        self.fields["role"].queryset = Lookup.objects.filter(type=role_type)
+
+
+@admin.register(MerchantMember)
+class MerchantMemberAdmin(admin.ModelAdmin):
+    form = MerchantMemberForm
+
+
+@admin.register(Merchant)
+class MerchantAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["owner"]
+    search_fields = ["name", "short_name", "code", "domain"]
