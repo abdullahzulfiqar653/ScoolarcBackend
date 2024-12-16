5b1bd2c238cc4ca24c64bd0f5523d646fc217cc6
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-12-16 13:18:49 +0500

    OTp and merchant config model added

--- src/api/common/contants.py
+++ src/api/common/contants.py
@@ -1,6 +1,8 @@
 MODEL_CODES = {
     "Lookup": "100",
+    "OTP": "101",
     "Merchant": "200",
     "Outlet": "201",
     "MerchantMember": "202",
+    "MerchantConfig": "203",
 }
--- src/api/models/__init__.py
+++ src/api/models/__init__.py
@@ -1,6 +1,15 @@
+from api.models.otp import OTP
 from api.models.lookup import Lookup
-from api.models.merchant import Merchant
 from api.models.outlet import Outlet
+from api.models.merchant import Merchant
 from api.models.merchant_member import MerchantMember
+from api.models.merchant_config import MerchantConfig
 
-__all__ = ["Lookup", "Merchant", "Outlet", "MerchantMember"]
+__all__ = [
+    "OTP",
+    "Lookup",
+    "Outlet",
+    "Merchant",
+    "MerchantConfig",
+    "MerchantMember",
+]
