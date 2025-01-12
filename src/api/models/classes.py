b1200161050694e0b1a91586da7aca73f527ad09
Shamasulhaq Python Developer <ulhaqshams924@gmail.com>
2025-01-13 03:48:55 +0500

    update models

--- src/api/models/classes.py
+++ src/api/models/classes.py
@@ -4,6 +4,12 @@ from api.models.abstract.base import BaseModel
 
 class Classes(BaseModel):
     name = models.CharField(max_length=255)
+    outlet = models.ForeignKey(
+        "api.Outlet",
+        on_delete=models.CASCADE,
+        related_name="outlet_classes",
+        null=True,
+    )
 
     def __str__(self):
         return self.name
