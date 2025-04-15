a78d63b15c20dd72bfc21cdee29cea6f34c24e03
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-04-15 13:42:08 +0500

    merchant id removed

--- src/api/serializers/outlet.py
+++ src/api/serializers/outlet.py
@@ -5,11 +5,7 @@ from api.serializers.merchant import MerchantSerializer
 
 class OutletSerializer(serializers.ModelSerializer):
     merchant = MerchantSerializer(read_only=True)
-    merchant_id = serializers.PrimaryKeyRelatedField(
-        source="merchant",
-        queryset=MerchantSerializer.Meta.model.objects.all(),
-        write_only=True,
-    )
+
     class Meta:
         model = Outlet
         fields = ("id", "name", "province", "city", "location", "code", "merchant",)
