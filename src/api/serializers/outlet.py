eb7948b1957f62531238d32c79151dc08f8a29ae
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-04-15 13:37:57 +0500

    merchant id is now readable

--- src/api/serializers/outlet.py
+++ src/api/serializers/outlet.py
@@ -13,7 +13,7 @@ class OutletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Outlet
         fields = ("id", "name", "province", "city", "location", "code", "merchant", "merchant_id")
-        read_only_fields = ("code",)
+        read_only_fields = ("code", "merchant_id",)
 
     def validate_name(self, name):
         outlet = self.context.get("request").merchant.outlets.filter(name=name)
