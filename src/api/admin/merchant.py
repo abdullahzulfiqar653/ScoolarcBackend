5e985ff0fa99d36ee52a090b36b88378763649bd
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-05-01 11:46:05 +0500

    merchant member issue resolved

--- src/api/admin/merchant.py
+++ src/api/admin/merchant.py
@@ -8,24 +8,9 @@ from api.models.member import Member
 admin.site.register(Outlet)
 
 
-class MemberForm(forms.ModelForm):
-    class Meta:
-        model = Member
-        fields = "__all__"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Filter the Lookup choices for the role field (filtering by "Role" type)
-        role_type = Lookup.objects.get(
-            name="Role"
-        )  # Assuming "Role" is the parent type
-        self.fields["role"].queryset = Lookup.objects.filter(type=role_type)
-
-
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    form = MemberForm
+    pass
 
 
 @admin.register(Merchant)
