1e4a011d6c3c8546df6cbc3a78f8ed8d14feb67b
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-05-09 15:03:54 +0500

    issue fixed for fetching parents

--- src/api/views/outlets/guardian.py
+++ src/api/views/outlets/guardian.py
@@ -16,9 +16,10 @@ class OutletParentsListAPIView(ListAPIView):
     search_fields = ["address", "city", "primary_phone", "first_name", "area", "email"]
 
     def get_queryset(self):
-        first_student = self.request.parent.guardian_students.first()
-        outlet = first_student.student_section.section_class.outlet
-        return Guardian.objects.filter(outlets=outlet)
+        # 103bFkC8jb6cpV7
+        # first_student = self.request.parent.guardian_students.first()
+        # outlet = first_student.student_section.section_class.outlet
+        return Guardian.objects.filter(outlets=self.request.outlet)
 
     @extend_schema(
         description="""
