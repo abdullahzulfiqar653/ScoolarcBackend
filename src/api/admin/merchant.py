6df1948eeb346267ebeb281e9520eeeb0ce6d82e
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-05-19 22:46:32 +0500

    guardian admin added

--- src/api/admin/merchant.py
+++ src/api/admin/merchant.py
@@ -4,6 +4,8 @@ from api.models.lookup import Lookup
 from api.models.merchant import Merchant
 from api.models.outlet import Outlet
 from api.models.member import Member
+from api.models.guardian import Guardian
+
 
 admin.site.register(Outlet)
 
@@ -20,6 +22,19 @@ class MemberAdmin(admin.ModelAdmin):
         "role",
     ]
     pass
+
+
+@admin.register(Guardian)
+class GuardianAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "mobile_notification_token",
+        "primary_phone",
+        "email",
+        "role",
+    ]
 
 
 @admin.register(Merchant)
